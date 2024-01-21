c2cd0ce24eedd03da4f18db1616ac45be13f1e0e
Callum Morris <me@callum.io>
2024-01-21 22:10:29 +0000

    feat: add styling for shad

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui/button";
+import { ThemeToggle } from "@/components/ui/theme";
 import Image from "next/image";
 
 export default function Home() {
@@ -6,6 +7,7 @@ export default function Home() {
     <main>
       <Button>Click me</Button>
       <Image src="/logo.svg" alt="Logo" width={200} height={200} />
+      <ThemeToggle />
     </main>
   );
 }
