5191e39f7a36c0007dcda4eebd77db84d0c9034a
Callum Morris <me@callum.io>
2024-01-22 19:02:18 +0000

    feat: begin work on initial layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,9 @@
-import { Button } from "@/components/ui/button";
-import { ThemeToggle } from "@/components/ui/theme";
-import Image from "next/image";
+import { SiteHeader } from '@/components/ui/site-header';
 
 export default function Home() {
   return (
-    <main>
-      <Button>Click me</Button>
-      <Image src="/logo.svg" alt="Logo" width={200} height={200} />
-      <ThemeToggle />
-    </main>
+    <div className="flex">
+      <SiteHeader />
+    </div>
   );
 }
---
+++ src/components/ui/mobile-nav.tsx
@@ -0,0 +1,72 @@
+'use client';
+
+import Link, { LinkProps } from 'next/link';
+import { useRouter } from 'next/navigation';
+import * as React from 'react';
+
+import { Button } from '@/components/ui/button';
+import { Logo } from '@/components/ui/logo';
+import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet';
+import { appConfig } from '@/config/app';
+import { cn } from '@/lib/utils';
+import { HamburgerMenuIcon } from '@radix-ui/react-icons';
+import { ScrollArea } from '@radix-ui/react-scroll-area';
+
+export function MobileNav() {
+  const [open, setOpen] = React.useState(false);
+
+  return (
+    <Sheet open={open} onOpenChange={setOpen}>
+      <SheetTrigger asChild>
+        <Button
+          variant="ghost"
+          className="mr-2 px-0 text-base hover:bg-transparent focus-visible:bg-transparent focus-visible:ring-0 focus-visible:ring-offset-0 md:hidden"
+        >
+          <HamburgerMenuIcon className="h-5 w-5" />
+
+          <span className="sr-only">Toggle Menu</span>
+        </Button>
+      </SheetTrigger>
+      <SheetContent side="left" className="pr-0">
+        <MobileLink
+          href="/"
+          className="flex items-center"
+          onOpenChange={setOpen}
+        >
+          <Logo className="mr-2 h-4 w-4" />
+          <span className="font-bold">{appConfig.name}</span>
+        </MobileLink>
+        <ScrollArea className="my-4 h-[calc(100vh-8rem)] pb-10 pl-6"></ScrollArea>
+      </SheetContent>
+    </Sheet>
+  );
+}
+
+interface MobileLinkProps extends LinkProps {
+  onOpenChange?: (open: boolean) => void;
+  children: React.ReactNode;
+  className?: string;
+}
+
+function MobileLink({
+  href,
+  onOpenChange,
+  className,
+  children,
+  ...props
+}: MobileLinkProps) {
+  const router = useRouter();
+  return (
+    <Link
+      href={href}
+      onClick={() => {
+        router.push(href.toString());
+        onOpenChange?.(false);
+      }}
+      className={cn(className)}
+      {...props}
+    >
+      {children}
+    </Link>
+  );
+}
