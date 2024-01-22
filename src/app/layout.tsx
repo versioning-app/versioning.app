acaf5f387a681c9e07ab9608a2bda28f99d1643f
Callum Morris <me@callum.io>
2024-01-22 19:02:18 +0000

    feat: begin work on initial layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,18 @@
-import { cn } from "@/lib/utils";
-import type { Metadata } from "next";
-import { Noto_Sans as FontSans } from "next/font/google";
+import { cn } from '@/lib/utils';
+import type { Metadata } from 'next';
+import { Noto_Sans as FontSans } from 'next/font/google';
 
-import { ThemeProvider } from "@/components/providers/theme-provider";
-import "../styles/globals.css";
+import { ThemeProvider } from '../../providers/theme-provider';
+import '../styles/globals.css';
 
 const fontSans = FontSans({
-  subsets: ["latin"], variable: "--font-sans",
+  subsets: ['latin'],
+  variable: '--font-sans',
 });
 
 export const metadata: Metadata = {
-  title: "versioning.app",
-  description: "Version management for your projects, made simple.",
+  title: 'versioning.app',
+  description: 'Version management for your projects, made simple.',
 };
 
 export default function RootLayout({
@@ -21,10 +22,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={cn(
-        "min-h-screen bg-background font-sans antialiased",
-        fontSans.variable
-      )}>
+      <body
+        className={cn(
+          'min-h-screen bg-background font-sans antialiased',
+          fontSans.variable
+        )}
+      >
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
---
+++ src/components/ui/logo.tsx
@@ -0,0 +1,16 @@
+import { cn } from '@/lib/utils';
+import Image, { ImageProps } from 'next/image';
+
+export const Logo = ({
+  className,
+  ...props
+}: Omit<ImageProps, 'src' | 'alt'>) => (
+  <Image
+    src="/logo.svg"
+    alt="versioning.app Logo"
+    width={60}
+    height={60}
+    className={cn('dark:invert inline-block align-middle')}
+    {...props}
+  />
+);
