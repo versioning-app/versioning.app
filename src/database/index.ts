6334ae2ed5713c998090366748da64fe8bedf2cd
Callum Morris <me@callum.io>
2024-01-23 18:33:58 +0000

    feat: add next auth

---
+++ src/database/index.ts
@@ -0,0 +1,9 @@
+import { neon } from '@neondatabase/serverless';
+import { drizzle } from 'drizzle-orm/neon-http';
+import { migrate } from 'drizzle-orm/neon-http/migrator';
+
+const sql = neon(process.env.DRIZZLE_DATABASE_URL!);
+
+export const db = drizzle(sql);
+
+await migrate(db, { migrationsFolder: 'drizzle' });
