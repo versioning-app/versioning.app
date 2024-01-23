88f5296c9e6f2709c5cddc8125cffe420dabde88
Callum Morris <me@callum.io>
2024-01-23 18:33:58 +0000

    feat: add next auth

---
+++ src/auth.ts
@@ -0,0 +1,22 @@
+import NextAuth from 'next-auth';
+
+import { DrizzleAdapter } from '@auth/drizzle-adapter';
+import Email from 'next-auth/providers/email';
+
+import { db } from '@/database';
+import type { NextAuthConfig } from 'next-auth';
+
+export const config = {
+  theme: {
+    logo: 'http://localhost:3000/logo-white-transparent.png',
+  },
+  adapter: DrizzleAdapter(db),
+  providers: [
+    Email({
+      server: process.env.EMAIL_SERVER,
+      from: process.env.EMAIL_FROM,
+    }),
+  ],
+} satisfies NextAuthConfig;
+
+export const { handlers, auth, signIn, signOut } = NextAuth(config);
