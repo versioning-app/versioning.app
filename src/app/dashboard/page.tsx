e0d155e33b552846d515a822cc563ec814b4b02b
Callum Morris <me@callum.io>
2024-02-02 23:00:59 +0000

    feat: add database schema for entity linking & logging

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,9 @@
 import { Navigation } from '@/config/nav';
-import { auth, clerkClient, currentUser } from '@clerk/nextjs';
+import { db } from '@/database/db';
+import { entities } from '@/database/schema';
+import { serverLogger } from '@/lib/logger/server';
+import { auth, clerkClient } from '@clerk/nextjs';
+import { and, eq } from 'drizzle-orm';
 import { redirect } from 'next/navigation';
 
 const getOrganization = async (organizationId: string | null | undefined) => {
@@ -12,21 +16,63 @@ const getOrganization = async (organizationId: string | null | undefined) => {
   });
 };
 
-export default async function Dashboard() {
-  const user = await currentUser();
+export const getEntity = async ({
+  userId,
+  orgId,
+}: {
+  userId: string;
+  orgId?: string;
+}) => {
+  const logger = serverLogger();
 
-  if (!user) {
+  const existing = await db
+    .selectDistinct()
+    .from(entities)
+    .where(
+      and(
+        eq(entities.type, orgId ? 'organization' : 'user'),
+        eq(entities.clerkId, orgId || userId)
+      )
+    );
+
+  // If the entity exists, return it
+  if (existing?.[0]) {
+    logger.info({ existing: existing[0] }, 'Entity exists, returning');
+    return existing[0];
+  }
+
+  logger.debug({ orgId, userId }, 'Entity does not exist, creating');
+
+  // Otherwise, create a new entity
+  const [newEntity] = await db
+    .insert(entities)
+    .values({
+      type: orgId ? 'organization' : 'user',
+      clerkId: orgId || userId,
+    })
+    .returning();
+
+  logger.info({ newEntity }, 'Entity created');
+
+  return newEntity;
+};
+
+export default async function Dashboard() {
+  const { orgId, userId } = auth();
+
+  if (!userId) {
     return redirect(Navigation.HOME);
   }
 
-  const { orgId } = auth();
+  // const organization = await getOrganization(orgId);
 
-  const organization = await getOrganization(orgId);
+  const entity = await getEntity({ userId, orgId });
 
   return (
     <div>
-      <p>Welcome to your dashboard, {user.firstName}</p>
-      {organization && <p>You are a member of {organization.name}</p>}
+      <p>Welcome to your dashboard</p>
+      {/* {organization && <p>You are a member of {organization.name}</p>} */}
+      {entity && <pre>{JSON.stringify(entity, null, 2)}</pre>}
     </div>
   );
 }
--- src/database/schema.ts
+++ src/database/schema.ts
@@ -1,1 +1,10 @@
+import { bigserial, pgEnum, pgTable, varchar } from 'drizzle-orm/pg-core';
 
+export const entityType = pgEnum('entity_type', ['user', 'organization']);
+
+export const entities = pgTable('entities', {
+  id: bigserial('id', { mode: 'number' }),
+  type: entityType('type'),
+  clerkId: varchar('clerk_id', { length: 255 }),
+  stripeCustomerId: varchar('stripe_customer_id', { length: 255 }),
+});
