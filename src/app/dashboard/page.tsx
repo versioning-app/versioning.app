59dc99ccb7b29c7d1a0f850a83bfaae4c57a77d6
Callum Morris <me@callum.io>
2024-02-02 23:31:43 +0000

    feat: seperate actions out

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,6 @@
+import { getEntity } from '@/actions/entity';
 import { Navigation } from '@/config/nav';
-import { db } from '@/database/db';
-import { entities } from '@/database/schema';
-import { serverLogger } from '@/lib/logger/server';
 import { auth, clerkClient } from '@clerk/nextjs';
-import { and, eq } from 'drizzle-orm';
 import { redirect } from 'next/navigation';
 
 const getOrganization = async (organizationId: string | null | undefined) => {
@@ -16,57 +13,14 @@ const getOrganization = async (organizationId: string | null | undefined) => {
   });
 };
 
-export const getEntity = async ({
-  userId,
-  orgId,
-}: {
-  userId: string;
-  orgId?: string;
-}) => {
-  const logger = serverLogger();
-
-  const existing = await db
-    .selectDistinct()
-    .from(entities)
-    .where(
-      and(
-        eq(entities.type, orgId ? 'organization' : 'user'),
-        eq(entities.clerkId, orgId || userId)
-      )
-    );
-
-  // If the entity exists, return it
-  if (existing?.[0]) {
-    logger.info({ existing: existing[0] }, 'Entity exists, returning');
-    return existing[0];
-  }
-
-  logger.debug({ orgId, userId }, 'Entity does not exist, creating');
-
-  // Otherwise, create a new entity
-  const [newEntity] = await db
-    .insert(entities)
-    .values({
-      type: orgId ? 'organization' : 'user',
-      clerkId: orgId || userId,
-    })
-    .returning();
-
-  logger.info({ newEntity }, 'Entity created');
-
-  return newEntity;
-};
-
 export default async function Dashboard() {
-  const { orgId, userId } = auth();
+  const { userId } = auth();
 
   if (!userId) {
     return redirect(Navigation.HOME);
   }
 
-  // const organization = await getOrganization(orgId);
-
-  const entity = await getEntity({ userId, orgId });
+  const entity = await getEntity();
 
   return (
     <div>
