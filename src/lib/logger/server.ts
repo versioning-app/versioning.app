1bd3c02335d6091ea665270661f0925a2dc13a54
Callum Morris <me@callum.io>
2024-02-02 23:31:43 +0000

    feat: seperate actions out

--- src/lib/logger/server.ts
+++ src/lib/logger/server.ts
@@ -2,11 +2,11 @@ import { getCorrelationId, getLogger } from '@/lib/logger';
 import { headers } from 'next/headers';
 
 export const serverLogger = () => {
-  const logger = getLogger();
+  let logger = getLogger();
 
   try {
     const correlationId = getCorrelationId(headers());
-    logger.child({ correlationId });
+    logger = logger.child({ correlationId });
   } catch {
     logger.warn('Failed to set correlationId in logger context.');
   }
