6c982d16a49cd2c7ea7a940937215e6096bf85da
Callum Morris <me@callum.io>
2024-02-05 22:54:57 +0000

    feat: force revalidate

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,12 +1,24 @@
+import { Navigation } from '@/config/navigation';
 import { ServiceFactory } from '@/services/service-factory';
 import { WorkspaceService } from '@/services/workspace.service';
 import { auth } from '@clerk/nextjs';
-import { unstable_noStore as noStore } from 'next/cache';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
 
-export default async function Dashboard() {
-  noStore();
-
+export default async function Dashboard({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string } & {
+    revalidate?: string;
+    ts?: string;
+  };
+}) {
   const { sessionClaims } = auth();
+
+  if (searchParams?.revalidate === 'true') {
+    revalidatePath(Navigation.DASHBOARD);
+    return redirect(Navigation.DASHBOARD);
+  }
 
   const workspaceId = await ServiceFactory.get(
     WorkspaceService
