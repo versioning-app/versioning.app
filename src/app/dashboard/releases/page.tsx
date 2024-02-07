4e93a12ed0b4c52152fa5480d22dcf33703aafbc
Callum Morris <me@callum.io>
2024-02-07 20:47:41 +0000

    feat: add initial big calendar component

--- src/app/dashboard/releases/page.tsx
+++ src/app/dashboard/releases/page.tsx
@@ -1,7 +1,10 @@
+import BigCalendar from '@/components/ui/big-calendar';
+
 export default async function Releases() {
   return (
     <div>
       <h1>Releases</h1>
+      <BigCalendar />
     </div>
   );
 }
