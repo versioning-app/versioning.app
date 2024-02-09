37786833a4b0c49b563e4f8172b513ef94cbd670
Callum Morris <me@callum.io>
2024-02-09 21:11:54 +0000

    fix: build and runtime

--- src/services/service-factory.ts
+++ src/services/service-factory.ts
@@ -6,15 +6,15 @@ export abstract class ServiceFactory {
   private static readonly services: Map<string, BaseService> = new Map();
 
   public static get<T extends BaseService>(service: new () => T): T {
-    const serviceName = service.name;
+    // const serviceName = service.name;
 
-    if (this.services.has(serviceName)) {
-      return this.services.get(serviceName) as T;
-    }
+    // if (this.services.has(serviceName)) {
+    //   return this.services.get(serviceName) as T;
+    // }
 
     const instance = new service();
 
-    this.services.set(serviceName, instance);
+    // this.services.set(serviceName, instance);
 
     return instance;
   }
