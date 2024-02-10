0b13eb327091a80ff90ba41cf9db69fcab899935
Callum Morris <me@callum.io>
2024-02-10 14:52:24 +0000

    feat: configure webhook support

---
+++ src/app/api/billing/webhooks/stripe/route.ts
@@ -0,0 +1,35 @@
+import { AppError } from '@/lib/error/app.error';
+import { ErrorCodes } from '@/lib/error/error-codes';
+import { serverLogger } from '@/lib/logger/server';
+import { ServiceFactory } from '@/services/service-factory';
+import { StripeService } from '@/services/stripe.service';
+
+export async function POST(request: Request) {
+  const stripeService = ServiceFactory.get(StripeService);
+
+  const body = await request.text();
+  const signature = request.headers.get('stripe-signature') as string;
+
+  let received = false;
+  try {
+    received = await stripeService.handleWebhookEvent(body, signature);
+  } catch (err: any) {
+    if (err instanceof AppError) {
+      return err.toResponse();
+    }
+
+    serverLogger({ source: 'api/billing/webhooks/stripe' }).error(
+      { err },
+      'Error handling webhook event'
+    );
+
+    const unknownError = new AppError(
+      'Unknown error occurred while handling webhook event',
+      ErrorCodes.UNKNOWN_ERROR
+    );
+
+    return unknownError.toResponse();
+  }
+
+  return new Response(JSON.stringify({ received }));
+}
--- src/lib/error/error-codes.ts
+++ src/lib/error/error-codes.ts
@@ -1,12 +1,62 @@
-export const ErrorCodes = {
-  RESOURCE_ALREADY_EXISTS: 'RESOURCE_ALREADY_EXISTS',
-  RESOURCE_NOT_FOUND: 'RESOURCE_NOT_FOUND',
-  UNHANDLED_ERROR: 'UNHANDLED_ERROR',
-  WORKSPACE_NOT_FOUND: 'WORKSPACE_NOT_FOUND',
-  USER_NOT_FOUND: 'USER_NOT_FOUND',
-  ORGANIZATION_NOT_FOUND: 'ORGANIZATION_NOT_FOUND',
-  INVALID_SESSION_CLAIMS: 'INVALID_SESSION_CLAIMS',
-  STRIPE_CUSTOMER_DELETED: 'STRIPE_CUSTOMER_DELETED',
-  STRIPE_CUSTOMER_NOT_LINKED: 'STRIPE_CUSTOMER_NOT_LINKED',
-  INTERNAL_MISCONFIGURATION: 'INTERNAL_MISCONFIGURATION',
+import { StatusCodes } from 'http-status-codes';
+
+export type ErrorCodeType = string;
+
+export type ErrorCode = {
+  type: ErrorCodeType;
+  statusCode: StatusCodes;
+};
+
+export const ErrorCodes: Record<string, ErrorCode> = {
+  // RESOURCE STATES
+  RESOURCE_ALREADY_EXISTS: {
+    type: 'RESOURCE_ALREADY_EXISTS',
+    statusCode: StatusCodes.CONFLICT,
+  },
+  RESOURCE_NOT_FOUND: {
+    type: 'RESOURCE_NOT_FOUND',
+    statusCode: StatusCodes.NOT_FOUND,
+  },
+  WORKSPACE_NOT_FOUND: {
+    type: 'WORKSPACE_NOT_FOUND',
+    statusCode: StatusCodes.NOT_FOUND,
+  },
+
+  // AUTHENTICATION
+  USER_NOT_FOUND: {
+    type: 'USER_NOT_FOUND',
+    statusCode: StatusCodes.NOT_FOUND,
+  },
+  ORGANIZATION_NOT_FOUND: {
+    type: 'ORGANIZATION_NOT_FOUND',
+    statusCode: StatusCodes.NOT_FOUND,
+  },
+  INVALID_SESSION_CLAIMS: {
+    type: 'INVALID_SESSION_CLAIMS',
+    statusCode: StatusCodes.UNAUTHORIZED,
+  },
+
+  // STRIPE
+  STRIPE_CUSTOMER_DELETED: {
+    type: 'STRIPE_CUSTOMER_DELETED',
+    statusCode: StatusCodes.NOT_FOUND,
+  },
+  STRIPE_CUSTOMER_NOT_LINKED: {
+    type: 'STRIPE_CUSTOMER_NOT_LINKED',
+    statusCode: StatusCodes.NOT_FOUND,
+  },
+  STRIPE_UNHANDLED_WEBHOOK_EVENT: {
+    type: 'STRIPE_UNHANDLED_WEBHOOK_EVENT',
+    statusCode: StatusCodes.BAD_REQUEST,
+  },
+
+  // GENERIC
+  INTERNAL_MISCONFIGURATION: {
+    type: 'INTERNAL_MISCONFIGURATION',
+    statusCode: StatusCodes.NOT_IMPLEMENTED,
+  },
+  UNHANDLED_ERROR: {
+    type: 'UNHANDLED_ERROR',
+    statusCode: StatusCodes.INTERNAL_SERVER_ERROR,
+  },
 } as const;
