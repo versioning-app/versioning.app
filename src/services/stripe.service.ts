5b18f78d8e3eb0ded04084fac6ac847013cb845c
Callum Morris <me@callum.io>
2024-02-10 14:52:24 +0000

    feat: configure webhook support

--- src/services/stripe.service.ts
+++ src/services/stripe.service.ts
@@ -5,8 +5,20 @@ import { stripe } from '@/lib/stripe/stripe';
 import { getURL } from '@/lib/utils';
 import { BaseService } from '@/services/base.service';
 import { WorkspaceService } from '@/services/workspace.service';
+import { Stripe } from 'stripe';
 
 export class StripeService extends BaseService {
+  public static WEBHOOK_EVENTS = new Set([
+    'product.created',
+    'product.updated',
+    'price.created',
+    'price.updated',
+    'checkout.session.completed',
+    'customer.subscription.created',
+    'customer.subscription.updated',
+    'customer.subscription.deleted',
+  ]);
+
   public constructor(
     private readonly workspaceService: WorkspaceService = new WorkspaceService()
   ) {
@@ -67,6 +79,9 @@ export class StripeService extends BaseService {
     const customer = await stripe.customers.create({
       name,
       email,
+      metadata: {
+        workspaceId: workspace.id,
+      },
     });
 
     this.logger.info(
@@ -94,5 +109,97 @@ export class StripeService extends BaseService {
       customer: workspace.stripeCustomerId,
       return_url: `${getURL()}/${Navigation.DASHBOARD_BILLING}`,
     });
+  }
+
+  public async handleWebhookEvent(
+    body: string,
+    signature: string
+  ): Promise<boolean> {
+    const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET;
+    let event: Stripe.Event;
+
+    try {
+      if (!signature || !webhookSecret) {
+        this.logger.error('Stripe Webhook signature or secret missing');
+
+        throw new AppError(
+          'Error processing stripe webhook',
+          ErrorCodes.STRIPE_WEBHOOK_ERROR
+        );
+      }
+
+      event = stripe.webhooks.constructEvent(body, signature, webhookSecret);
+
+      this.logger.debug({ event }, 'Stripe Webhook event constructed');
+    } catch (err: any) {
+      this.logger.error({ err, body }, `Stripe Webhook Error: ${err.message}`);
+
+      throw new AppError(
+        'Error processing stripe webhook',
+        ErrorCodes.STRIPE_WEBHOOK_ERROR
+      );
+    }
+
+    if (StripeService.WEBHOOK_EVENTS.has(event.type)) {
+      this.logger.debug({ event }, 'Processing Stripe Webhook event');
+
+      try {
+        switch (event.type) {
+          case 'product.created':
+          case 'product.updated':
+            // await upsertProductRecord(event.data.object as Stripe.Product);
+            break;
+          case 'price.created':
+          case 'price.updated':
+            // await upsertPriceRecord(event.data.object as Stripe.Price);
+            break;
+          case 'customer.subscription.created':
+          case 'customer.subscription.updated':
+          case 'customer.subscription.deleted':
+            // const subscription = event.data.object as Stripe.Subscription;
+            // await manageSubscriptionStatusChange(
+            //   subscription.id,
+            //   subscription.customer as string,
+            //   event.type === 'customer.subscription.created'
+            // );
+            break;
+          case 'checkout.session.completed':
+            // const checkoutSession = event.data.object as Stripe.Checkout.Session;
+            // if (checkoutSession.mode === 'subscription') {
+            //   const subscriptionId = checkoutSession.subscription;
+            //   await manageSubscriptionStatusChange(
+            //     subscriptionId as string,
+            //     checkoutSession.customer as string,
+            //     true
+            //   );
+            // }
+            break;
+          default:
+            throw new AppError(
+              'No webhook handler defined for stripe event',
+              ErrorCodes.STRIPE_UNHANDLED_WEBHOOK_EVENT
+            );
+        }
+      } catch (error) {
+        this.logger.error({ error }, 'Stripe Webhook handler failed');
+
+        // Handle our own errors, otherwise throw generic error
+        if (error instanceof AppError) {
+          throw error;
+        }
+
+        throw new AppError(
+          'Error processing stripe webhook',
+          ErrorCodes.STRIPE_WEBHOOK_ERROR
+        );
+      }
+    } else {
+      this.logger.warn(
+        { event },
+        'Received Stripe Webhook event type that is not defined in our recognized event list'
+      );
+    }
+
+    return true;
   }
 }
