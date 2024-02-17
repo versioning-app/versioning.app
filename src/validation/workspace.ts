89ed16ed2273a1c42bb92c33a718e3e1de34c602
Callum Morris <me@callum.io>
2024-02-17 09:58:37 +0000

    fix: small issues

--- src/validation/workspace.ts
+++ src/validation/workspace.ts
@@ -1,5 +1,9 @@
 import { z } from 'zod';
 
 export const changeSlugSchema = z.object({
-  slug: z.string().toLowerCase().min(3).max(32),
+  slug: z
+    .string()
+    .min(3)
+    .max(32)
+    .regex(/^[a-z0-9]+(?:-[a-z0-9]+)*$/),
 });
