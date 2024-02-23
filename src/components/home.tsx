b43c135362f850da2e302a32d0c604acd9d64944
Callum Morris <me@callum.io>
2024-02-23 21:22:45 +0000

    feat: add support for offline postgres

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,8 +1,2 @@
-import { Footer, Header } from '@/components/common/home-components';
-import { Logo } from '@/components/common/logo';
-import { appConfig } from '@/config/app';
-import { Navigation } from '@/config/navigation';
-import { CheckIcon } from '@radix-ui/react-icons';
-import Link from 'next/link';
 
 
--- src/database/db.ts
+++ src/database/db.ts
@@ -1,6 +1,15 @@
 import { neon } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http';
 
+import { neonConfig } from '@neondatabase/serverless';
+
+if (process.env.USE_OFFLINE_DATABASE) {
+  // https://github.com/neondatabase/serverless/issues/33
+  // https://github.com/TimoWilhelm/local-neon-http-proxy
+  neonConfig.fetchEndpoint = (host) =>
+    `https://${host}:${host === 'db.localtest.me' ? 4444 : 443}/sql`;
+}
+
 const sql = neon(process.env.DATABASE_URL!);
 
 export const db = drizzle(sql);
