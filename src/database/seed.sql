f84e5dbdc6752229318f80ac3a5e36b813408f57
Callum Morris <me@callum.io>
2024-02-24 09:23:01 +0000

    chore: update seed script

--- src/database/seed.sql
+++ src/database/seed.sql
@@ -2,7 +2,7 @@
 TRUNCATE workspaces CASCADE;
 
 INSERT INTO workspaces (id, type, slug, clerk_id) 
-VALUES ('workspace_1', 'organization', 'example_workspace', 'org_abc123');
+VALUES ('workspace_1', 'organization', 'testing_workspace', 'org_2bVbNW03wQLl3yjNIOZgV1JIovU');
 
 INSERT INTO members (id, clerk_id, workspace_id)
 VALUES 
