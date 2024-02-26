6247ab8ac6a46249638a4e5aa4db7cd803467ff3
Callum Morris <me@callum.io>
2024-02-26 19:37:53 +0000

    feat: use generic page for environments page

--- src/app/(dashboard)/[slug]/environments/page.tsx
+++ src/app/(dashboard)/[slug]/environments/page.tsx
@@ -1,18 +1,14 @@
-import { prettyPrint } from '@/lib/utils';
+import { EnvironmentList } from '@/components/dashboard/lists/environment';
 import { EnvironmentsService } from '@/services/environments.service';
 import { ServiceFactory } from '@/services/service-factory';
 
-export default async function EnvironmentsPage() {
+export default async function EnvironmentsPage({
+  params: { slug },
+}: {
+  params: { slug: string };
+}) {
   const environments =
     await ServiceFactory.get(EnvironmentsService).getEnvironments();
 
-  return (
-    <div>
-      <div className="flex">
-        <div className="flex-1">
-          <pre>{prettyPrint(environments)}</pre>
-        </div>
-      </div>
-    </div>
-  );
+  return <EnvironmentList slug={slug} environments={environments} />;
 }
