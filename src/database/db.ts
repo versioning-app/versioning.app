82be36728854db9f6599a96c7134d4c587457494
Callum Morris <me@callum.io>
2024-05-01 23:49:01 +0100

    feat: provide options to use a pre-built neon image (note: it could be slow)

--- src/database/db.ts
+++ src/database/db.ts
@@ -1,16 +1,8 @@
-import { neon } from '@neondatabase/serverless';
-import { drizzle } from 'drizzle-orm/neon-http';
 import * as schema from '@/database/schema';
+import { drizzle } from 'drizzle-orm/neon-http';
 
-import { neonConfig } from '@neondatabase/serverless';
+import { buildNeonClient } from '@/database/client';
 
-if (process.env.USE_OFFLINE_DATABASE) {
-  // https://github.com/neondatabase/serverless/issues/33
-  // https://github.com/TimoWilhelm/local-neon-http-proxy
-  neonConfig.fetchEndpoint = (host) =>
-    `https://${host}:${host === 'db.localtest.me' ? 4444 : 443}/sql`;
-}
-
-const sql = neon(process.env.DATABASE_URL!);
+const sql = buildNeonClient(process.env.DATABASE_URL!);
 
 export const db = drizzle(sql, { schema });
