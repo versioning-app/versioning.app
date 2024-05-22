25456fec9866d8750b7821ed9aec1ffde8a04a2a
Callum Morris <me@callum.io>
2024-05-22 20:04:42 +0100

    feat: upgrade libs

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,6 +1,10 @@
 import type { Config } from 'drizzle-kit';
 
 export default {
+  dialect: 'postgresql',
   schema: ['src/database/schema.ts'],
   out: 'src/database/migrations',
+  dbCredentials: {
+    url: process.env.DATABASE_URL!,
+  },
 } satisfies Config;
