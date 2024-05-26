4e8afee1138203ef3867e9480c0696ac8c84bc64
Callum Morris <me@callum.io>
2024-05-27 00:09:50 +0100

    feat: generic has hasDependents plus other re-work

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config) => {
-    // this will override the experiments
-    config.experiments = { ...config.experiments, topLevelAwait: true };
-    // this will just update topLevelAwait property of config.experiments
-    // config.experiments.topLevelAwait = true
-    return config;
-  },
   experimental: {
     serverMinification: false,
     serverComponentsExternalPackages: ['pino', 'pino-pretty'],
