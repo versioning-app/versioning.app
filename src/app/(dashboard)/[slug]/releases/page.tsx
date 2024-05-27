f9937206c39c7b51e1a2078de47f04542928001b
Callum Morris <me@callum.io>
2024-05-27 23:14:14 +0100

    feat: migrate to fullcalendar

--- src/app/(dashboard)/[slug]/releases/page.tsx
+++ src/app/(dashboard)/[slug]/releases/page.tsx
@@ -1,15 +1,26 @@
-import BigCalendar from '@/components/ui/big-calendar';
+import Calendar from '@/components/dashboard/calendar';
+import { ReleaseService } from '@/services/release.service';
+import { get } from '@/services/service-factory';
+import { EventInput } from '@fullcalendar/core/index.js';
 
 export default async function Releases() {
+  const releases = await get(ReleaseService).findAll();
+
+  const events = releases.map(
+    (release): EventInput => ({
+      title: release.version,
+      allDay: true,
+      date: release.date?.toISOString(),
+    }),
+  );
+
   return (
-    <div>
+    <div className="h-full mb-10">
       <h1>Releases</h1>
-      <BigCalendar
-        datePickerProps={{
-          weekStartsOn: 1,
-          showOutsideDays: true,
-        }}
-      />
+
+      <div className="h-full">
+        <Calendar events={events ?? []} height={'100%'} />
+      </div>
     </div>
   );
 }
