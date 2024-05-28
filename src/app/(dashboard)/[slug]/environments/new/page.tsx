30ace48ac64e2b805506679a7db4a1bee4257d62
Callum Morris <me@callum.io>
2024-05-28 23:47:25 +0100

    feat: general improvements to developer dx and reduce code dupe

--- src/app/(dashboard)/[slug]/environments/new/page.tsx
+++ src/app/(dashboard)/[slug]/environments/new/page.tsx
@@ -1,18 +1,3 @@
-import { CreateEnvironmentForm } from '@/components/dashboard/forms/create-environment';
-import { EnvironmentTypesService } from '@/services/environment-types.service';
-import { get } from '@/services/service-factory';
+import NewEnvironment from '@/app/(dashboard)/[slug]/environments/create';
 
-export default async function NewEnvironmentPage() {
-  const environmentTypesService = get(EnvironmentTypesService);
-  const environmentTypes = await environmentTypesService.findAll();
-
-  if (!environmentTypes) {
-    return <div>Please create an environment type first</div>;
-  }
-
-  return (
-    <div>
-      <CreateEnvironmentForm environmentTypes={environmentTypes} />
-    </div>
-  );
-}
+export default NewEnvironment;
--- src/app/(dashboard)/[slug]/releases/@modal/(.)new/page.tsx
+++ src/app/(dashboard)/[slug]/releases/@modal/(.)new/page.tsx
@@ -1,21 +1,3 @@
-import { CreateReleaseForm } from '@/components/dashboard/forms/create-release';
-import { PageDialog } from '@/components/dashboard/page-dialog';
-import { DialogContent } from '@/components/ui/dialog';
-import { ReleaseStrategiesService } from '@/services/release-strategies.service';
-import { get } from '@/services/service-factory';
+import NewRelease from '@/app/(dashboard)/[slug]/releases/create';
 
-export default async function NewReleaseStrategyModal() {
-  const releaseStrategies = await get(ReleaseStrategiesService).findAll();
-
-  if (!releaseStrategies) {
-    return <div>Please create a release strategy first</div>;
-  }
-
-  return (
-    <PageDialog>
-      <DialogContent>
-        <CreateReleaseForm releaseStrategies={releaseStrategies} />
-      </DialogContent>
-    </PageDialog>
-  );
-}
+export default NewRelease;
