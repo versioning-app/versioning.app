da3b7d8185c3cdae768e7cdd4f450b140d8ca6ee
Callum Morris <me@callum.io>
2024-05-29 19:19:13 +0100

    feat: refactor more pages to use modal logic

--- src/app/(dashboard)/[slug]/environment-types/@modal/(.)new/page.tsx
+++ src/app/(dashboard)/[slug]/environment-types/@modal/(.)new/page.tsx
@@ -1,13 +1,3 @@
-import { CreateEnvironmentTypeForm } from '@/components/dashboard/forms/create-environment-type';
-import { PageDialog } from '@/components/dashboard/page-dialog';
-import { DialogContent } from '@/components/ui/dialog';
+import NewEnvironmentType from '@/app/(dashboard)/[slug]/environment-types/create';
 
-export default async function NewEnvironmentModal() {
-  return (
-    <PageDialog>
-      <DialogContent>
-        <CreateEnvironmentTypeForm />
-      </DialogContent>
-    </PageDialog>
-  );
-}
+export default NewEnvironmentType;
--- src/app/(dashboard)/[slug]/release-strategies/layout.tsx
+++ src/app/(dashboard)/[slug]/release-strategies/layout.tsx
@@ -1,9 +1,7 @@
-import { Button } from '@/components/ui/button';
-import { Navigation, dashboardRoute } from '@/config/navigation';
-import { Plus } from 'lucide-react';
-import Link from 'next/link';
+import DashboardPageLayout from '@/components/dashboard/layouts/page-layout';
+import { Navigation } from '@/config/navigation';
 
-export default function ReleaseStrategiesLayout({
+export default function ReleaseStrategyLayout({
   children,
   modal,
   params: { slug },
@@ -13,37 +11,13 @@ export default function ReleaseStrategiesLayout({
   params: { slug: string };
 }) {
   return (
-    <>
-      <div className="h-full">
-        <div className="flex">
-          <div className="flex-1">
-            <Link
-              href={dashboardRoute(
-                slug,
-                Navigation.DASHBOARD_RELEASE_STRATEGIES,
-              )}
-            >
-              <h1 className="text-2xl">Release Strategies</h1>
-            </Link>
-          </div>
-          <div className="mb-4">
-            <Link
-              href={dashboardRoute(
-                slug,
-                Navigation.DASHBOARD_RELEASE_STRATEGIES_NEW,
-              )}
-              className="flex items-center"
-            >
-              <Button className="mr-2">
-                <Plus className="w-4 h-4 mr-2" />
-                New release strategy
-              </Button>
-            </Link>
-          </div>
-        </div>
-        <div className="flex flex-col mt-4 h-full">{children}</div>
-      </div>
-      {modal}
-    </>
+    <DashboardPageLayout
+      slug={slug}
+      page={children}
+      modal={modal}
+      resource="Release Strategy"
+      href={Navigation.DASHBOARD_RELEASE_STRATEGIES}
+      createHref={Navigation.DASHBOARD_RELEASE_STRATEGIES_NEW}
+    />
   );
 }
