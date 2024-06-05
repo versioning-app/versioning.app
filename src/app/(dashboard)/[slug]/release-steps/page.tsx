8e6381e8eaaf394948410e62b4bab84ab9511828
Callum Morris <me@callum.io>
2024-06-05 23:32:28 +0100

    feat: add release steps

---
+++ src/app/(dashboard)/[slug]/release-steps/page.tsx
@@ -0,0 +1,24 @@
+import { deleteReleaseStepAction } from '@/actions/release';
+import { List } from '@/components/dashboard/lists/list-item';
+import { Navigation, dashboardRoute } from '@/config/navigation';
+import { ReleaseStepService } from '@/services/release-steps.service';
+import { get } from '@/services/service-factory';
+
+export default async function ReleaseStrategyStepsPage({
+  params: { slug },
+}: {
+  params: { slug: string };
+}) {
+  const releaseSteps = await get(ReleaseStepService).findAll();
+
+  return (
+    <List
+      createLink={dashboardRoute(slug, Navigation.DASHBOARD_RELEASE_STEPS)}
+      resourceName="Release Step"
+      resources={releaseSteps}
+      actions={{
+        delete: deleteReleaseStepAction,
+      }}
+    />
+  );
+}
