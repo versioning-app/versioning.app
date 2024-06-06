3214453da5c3a1bcc799d45658b5c3a5c2b76d60
Callum Morris <me@callum.io>
2024-06-07 00:32:40 +0100

    feat: add initial releases view

--- src/providers/providers.tsx
+++ src/providers/providers.tsx
@@ -3,11 +3,14 @@
 import { NextUIProvider } from '@nextui-org/react';
 import { ThemeProvider as NextThemesProvider } from 'next-themes';
 import { type ThemeProviderProps } from 'next-themes/dist/types';
+import { ReactFlowProvider } from 'reactflow';
 
 export function AppProviders({ children, ...props }: ThemeProviderProps) {
   return (
     <NextUIProvider>
-      <NextThemesProvider {...props}>{children}</NextThemesProvider>
+      <NextThemesProvider {...props}>
+        <ReactFlowProvider>{children}</ReactFlowProvider>
+      </NextThemesProvider>
     </NextUIProvider>
   );
 }
