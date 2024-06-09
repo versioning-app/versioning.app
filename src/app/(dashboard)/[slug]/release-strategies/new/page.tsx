46ad256f42b13c6291ee74134f59d1ae7327f413
Callum Morris <me@callum.io>
2024-06-09 22:43:55 +0100

    feat: add release overview view and small tweaks

--- src/app/(dashboard)/[slug]/release-strategies/new/page.tsx
+++ src/app/(dashboard)/[slug]/release-strategies/new/page.tsx
@@ -1,3 +1,3 @@
-import NewReleaseStrategy from '@/app/(dashboard)/[slug]/environments/create';
+import NewReleaseStrategy from '@/app/(dashboard)/[slug]/release-strategies/create';
 
 export default NewReleaseStrategy;
