fb876f494d1bf3d4afdcc72b81934e8693f7431e
Callum Morris <me@callum.io>
2024-06-12 23:46:23 +0100

    feat: add api route with own middleware and authentication

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,10 @@
 import { StorageKeys } from '@/config/storage';
 import { generateRequestId } from '@/lib/utils';
+import { apiMiddleware } from '@/middleware/api';
 import { get } from '@/services/service-factory';
 import { WorkspaceService } from '@/services/workspace.service';
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 
 const isPublicRoute = createRouteMatcher([
   '/',
@@ -13,12 +14,18 @@ const isPublicRoute = createRouteMatcher([
   '/api/billing/webhooks/stripe',
 ]);
 
+const isApiRoute = createRouteMatcher(['/api/v1/(.*)']);
+
 export default clerkMiddleware(async (auth, req, event) => {
   // Generate request ID for each request
   req.headers.set(StorageKeys.REQUEST_ID_HEADER_KEY, generateRequestId());
 
   if (isPublicRoute(req)) {
     return NextResponse.next();
+  }
+
+  if (isApiRoute(req)) {
+    return apiMiddleware(req);
   }
 
   auth().protect();
