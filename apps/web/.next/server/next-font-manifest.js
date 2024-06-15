c9f420a7928041301998ccde6f55d8bad5ee3469
Callum Morris <me@callum.io>
2024-06-15 21:53:10 +0100

    feat: remove .next

--- apps/web/.next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST='{"pages":{},"app":{"/Users/cal/Development/versioningapp/apps/web/src/app/layout":["static/media/0e790e04fd40ad16-s.p.woff2"]},"appUsingSizeAdjust":true,"pagesUsingSizeAdjust":false}';
