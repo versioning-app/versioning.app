8341cc51e114ba79063e8d638901728f584a543d
Callum Morris <me@callum.io>
2024-06-15 21:52:30 +0100

    feat: ignore files

--- apps/web/.next/server/next-font-manifest.js
+++ apps/web/.next/server/next-font-manifest.js
@@ -1,1 +1,1 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{},\"appUsingSizeAdjust\":false,\"pagesUsingSizeAdjust\":false}"
+self.__NEXT_FONT_MANIFEST='{"pages":{},"app":{"/Users/cal/Development/versioningapp/apps/web/src/app/layout":["static/media/0e790e04fd40ad16-s.p.woff2"]},"appUsingSizeAdjust":true,"pagesUsingSizeAdjust":false}';
