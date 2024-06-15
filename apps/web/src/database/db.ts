176341621315efb6c93b0c776ee2de1647e91389
Callum Morris <me@callum.io>
2024-06-15 22:01:15 +0100

    fix: db env vars

--- apps/web/src/database/db.ts
+++ apps/web/src/database/db.ts
@@ -3,6 +3,6 @@ import { drizzleLogger } from '@/database/logger';
 import * as schema from '@/database/schema';
 import { drizzle } from 'drizzle-orm/neon-http';
 
-const sql = buildNeonClient(process.env.APP_DATABASE_URL!);
+const sql = buildNeonClient(process.env.DATABASE_URL!);
 
 export const db = drizzle(sql, { schema, logger: drizzleLogger });
