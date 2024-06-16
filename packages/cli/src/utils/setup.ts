63a4d3be45510543ac958e43d460b19ab43ec65d
Callum Morris <me@callum.io>
2024-06-16 20:09:13 +0100

    feat: init cli

---
+++ packages/cli/src/utils/setup.ts
@@ -0,0 +1,26 @@
+// import { isError } from '@/utils/error';
+// import epipebomb from 'epipebomb';
+
+import { isError } from '../utils/error.js';
+
+const checkCwdExists = () => {
+	try {
+		// Test to see if cwd has been deleted before
+		// importing 3rd party packages that might need cwd.
+		process.cwd();
+	} catch (err: unknown) {
+		if (isError(err) && err.message.includes('uv_cwd')) {
+			// eslint-disable-next-line no-console
+			console.error('Error: The current working directory does not exist.');
+			process.exit(1);
+		}
+	}
+};
+
+export const setup = () => {
+	// Fix for EPIPE errors when piping output to other commands
+	// Taken from vercel cli
+	// epipebomb();
+
+	checkCwdExists();
+};
