66fe60166e01434b791fb1f298f288c43afe78e7
Callum Morris <me@callum.io>
2024-06-16 21:24:51 +0100

    feat: tidy up cli

--- packages/cli/src/cli.tsx
+++ packages/cli/src/cli.tsx
@@ -1,8 +1,11 @@
 #!/usr/bin/env node
 import Pastel from 'pastel';
+import { setup } from '@/utils/setup';
 
 const app = new Pastel({
 	importMeta: import.meta,
 });
+
+setup();
 
 await app.run();
