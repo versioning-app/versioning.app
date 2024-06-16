fa08cbfbdde5d2ea4904dff32a8da469c85d749f
Callum Morris <me@callum.io>
2024-06-16 20:09:13 +0100

    feat: init cli

---
+++ packages/cli/tsup.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+	entry: ["src/**/*.ts", "src/**/*.tsx"],
+	sourcemap: true,
+	format: ["esm"],
+	target: "es2022",
+	clean: true,
+});
