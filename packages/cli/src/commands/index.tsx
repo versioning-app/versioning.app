fa76c34550c2efea497684a045fa7c62d22e6885
Callum Morris <me@callum.io>
2024-06-23 23:09:47 +0100

    feat: add webhooks support to cli

--- packages/cli/src/commands/index.tsx
+++ packages/cli/src/commands/index.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import { Header } from '@/components/header';
 import { Text } from 'ink';
+import Gradient from 'ink-gradient';
+import React from 'react';
 import zod from 'zod';
 
-export const options = zod.object({
-	name: zod.string().default('Stranger').describe('Name'),
-});
+export const options = zod.object({});
 
 type Props = {
 	options: zod.infer<typeof options>;
@@ -12,10 +12,11 @@ type Props = {
 
 export default function Index({ options }: Props) {
 	return (
-		<Text>
-			<Text color="cyan" bold={true}>
-				versioning.app
+		<>
+			<Header />
+			<Text color="green">
+				Run <Gradient name="mind">versioning help</Gradient> to see available
 			</Text>
-		</Text>
+		</>
 	);
 }
