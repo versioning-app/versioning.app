f27b47f9e213767160b757ed9cf60489250896dc
Callum Morris <me@callum.io>
2024-07-03 21:50:28 +0100

    feat: change dots

--- packages/cli/src/commands/webhooks.tsx
+++ packages/cli/src/commands/webhooks.tsx
@@ -35,7 +35,7 @@ export default function Webhooks({ options }: Props) {
 	return (
 		<>
 			<Header />
-			{!response ? <Spinner type="dots5" /> : null}
+			{!response ? <Spinner type="dots4" /> : null}
 			{response ? <Text>{JSON.stringify(response?.data, null, 2)}</Text> : null}
 		</>
 	);
