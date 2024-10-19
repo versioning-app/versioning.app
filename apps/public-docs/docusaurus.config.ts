ddd6b04f300d5a08a9289e42b14e07cc6ad55ba7
Callum Morris <me@callum.io>
2024-10-19 18:26:31 +0100

    feat: add docs

---
+++ apps/public-docs/docusaurus.config.ts
@@ -0,0 +1,97 @@
+import { themes as prismThemes } from "prism-react-renderer";
+import type { Config } from "@docusaurus/types";
+import type * as Preset from "@docusaurus/preset-classic";
+
+const config: Config = {
+  title: "versioning.app | docs",
+  tagline: "Documentation for versioning.app",
+  favicon: "img/favicon.ico",
+
+  // Set the production url of your site here
+  url: "https://docs.versioning.app",
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: "/",
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: "versioning.app", // Usually your GitHub org/user name.
+  projectName: "versioning.app", // Usually your repo name.
+
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en"],
+  },
+
+  presets: [
+    [
+      "classic",
+      {
+        docs: {
+          sidebarPath: "./sidebars.ts",
+        },
+        blog: {
+          showReadingTime: true,
+          feedOptions: {
+            type: ["rss", "atom"],
+            xslt: true,
+          },
+          // Useful options to enforce blogging best practices
+          onInlineTags: "warn",
+          onInlineAuthors: "warn",
+          onUntruncatedBlogPosts: "warn",
+        },
+        theme: {
+          customCss: "./src/css/custom.css",
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    // Replace with your project's social card
+    navbar: {
+      title: "versioning.app",
+      logo: {
+        alt: "versioning.app logo",
+        src: "img/logo.svg",
+      },
+      items: [
+        { to: "/docs", label: "Docs", position: "left" },
+        { to: "/blog", label: "Blog", position: "left" },
+        {
+          href: "https://github.com/versioning-app",
+          label: "GitHub",
+          position: "right",
+        },
+      ],
+    },
+    footer: {
+      style: "dark",
+      links: [
+        {
+          title: "Community",
+          items: [
+            {
+              label: "Twitter/X",
+              href: "https://x.com/versioningapp",
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} versioning.app`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
